fb2f04599428b3fa3fc27fa90fdc7183edcb2ea5
fearless40 <spivack@outlook.com>
2015-08-25 18:41:50 +0000

    Added some windows helper classes. Simple wrappers. Deciding to not use the std::mutex.

---
+++ Gravwell 3/CriticalSection.h
@@ -0,0 +1,44 @@
+
+namespace Util {
+
+
+
+class CriticalSection
+{
+    public:
+    static const int DefaultSpinCount = 400;
+    
+    protected:
+    CRITICAL_SECTION mSection;
+    
+    public:
+    CriticalSection( int spinCount ) {
+        InitalizeCriticalSectionAndSpinCount( &mSection, spinCount );
+    }
+    CriticalSection( ) {
+        InitalizeCriticalSectionAndSpinCount( &mSection, DefaultSpinCount );
+    }
+
+    ~CriticalSection() {
+        DeleteCriticalSection( &mSection );
+    }
+    
+    void setSpinCount( int spinCount ) {
+        SetCriticalSectionSpinCount( &mSection, spinCount );
+    }
+    
+    void lock() {
+        EnterCriticalSection( &mSection );
+    }
+    
+    void unlock() {
+        LeaveCriticalSection( &mSection );
+    }
+    
+    void tryLock() {
+        TryEnterCriticalSection( &mSection );
+    }
+};
+
+typedef LightMutex CriticalSection;
+};
