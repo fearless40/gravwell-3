9fa0bc1c1ba8f4dc5d6e58ba2c039da43291c493
fearless40 <spivack@outlook.com>
2015-08-26 03:02:51 +0000

    Updated documentation. Still needs some more work.

--- Jobs.h
+++ Jobs.h
@@ -3,7 +3,10 @@
 namespace Util {
 namespace Work {    
     
-   
+   /// Work Flow / ownership of memory
+   // 1. Call new Job and then set it up. Once done send the Job pointer into Jobs. 
+   // 2. Now Jobs owns the job and memory associated with it. The user must not use the pointer as it is now invalid.
+   // 3. 
 
     class Jobs
     {
@@ -23,13 +26,19 @@ namespace Work {
     	
         Jobs();
         
+        // Call backs
         static void JobSubmitDependentWork( WorkData data );
         
+        // Background Thread that cleans mDep and submits Job that all dependencies have been met
+        static void SubmitJobsThatDependenciesHaveBeenMet( WorkData * data )
+        
+        // Signal that a job is complete (handle scheduling backgground thread)
         void JobDependentWorkCompleted( Job * job );
     
     public:
         Jobs( ThreadPool & pool );
     
+        // Submit work without any dependencies 
     	void createSubmit(WorkFunction func, void * inData, void * outData);
     
     	// Also will add all the dependents from the job to the waiting array
